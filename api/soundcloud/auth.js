7a523c88707ea2d2066a5b24afb15d95fee1bdd4
not-the-singer <info@not-the-singer.com>
2025-07-14 22:04:38 +1000

    Create auth.js

---
+++ api/soundcloud/auth.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const clientId = process.env.SOUNDCLOUD_CLIENT_ID;
+  const redirectUri = 'https://not-the-singer-api.vercel.app/api/soundcloud/callback';
+  
+  const authUrl = `https://secure.soundcloud.com/connect?` +
+    `client_id=${clientId}&` +
+    `redirect_uri=${encodeURIComponent(redirectUri)}&` +
+    `response_type=code&` +
+    `scope=non-expiring`;
+  
+  res.redirect(authUrl);
+}
